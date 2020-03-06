dd63b1f5696d84f122021b713051840183c9ded4
lihongyu12 <2641889026@qq.com>
2020-03-06 11:35:42 +0800

    添加小程序文件

---
+++ zuoye/xiaochengxu/pages/A/A.js
@@ -0,0 +1,75 @@
+// pages/A/A.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    iconSize: [20, 30, 40, 50, 60, 25],
+    iconColor: [
+      '#000'
+    ],
+    iconType: [
+      'search'
+    ],
+    arr:['湿垃圾','干垃圾','可回收物','有害垃圾'],
+    arr2: ['CoCo的青稞', 'CoCo的青稞', 'CoCo的青稞', 'CoCo的青稞', 'CoCo的青稞', 'CoCo的青稞', 'CoCo的青稞'],
+    colors: ['#97715e', '#2d2926', '#004072','#ff4236']
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function(options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function() {
+
+  }
+})
